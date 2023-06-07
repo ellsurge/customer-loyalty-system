b22f545b239e125b3991d891309e403533a42fc9
ellsurge <iorsasesesugh@gmail.com>
2023-06-07 17:54:42 +0100

    removed auth

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,10 @@
 import { Box, Center, Container, HStack, Link, Text } from "@chakra-ui/react"
 
-interface NavBarProp{
-    auth :React.ReactNode
-}
 
-export default function NavBar({auth}:NavBarProp){
+export default function NavBar(){
     return (
         <Box 
-        bg="#160A01"
+        bg="#160A01" 
         color="#ffffff"
         height="100%"
         p={4}
@@ -18,7 +15,7 @@ export default function NavBar({auth}:NavBarProp){
                 <Link>home</Link>
                 <Link></Link>
                 <Link></Link>
-                {auth?`<Link>signup</Link>`:''}
+                {/* {auth?`<Link>signup</Link>`:''} */}
            </Center>
            </HStack>
 
